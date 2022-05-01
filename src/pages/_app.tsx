e17e38b0253377f31d5eddabab92e9ed555ae1f5
devharris7 <harris.wang@mail.com>
2022-05-01 14:48:39 +1000

    feat: add things

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,45 @@
-import "../styles/tailwind.css";
-import type { AppProps } from "next/app";
+import React from "react";
+import App, { AppContext } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { colors } from "../styles";
+import { Provider } from "mobx-react";
+import { fetchInitialStoreState, DataStore } from "../stores/DataStore";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const theme = extendTheme({ colors });
+
+// https://medium.com/@borisdedejski/next-js-mobx-and-typescript-boilerplate-for-beginners-9e28ac190f7d
+class MyApp extends App {
+  state = {
+    dataStore: new DataStore(),
+  };
+
+  // Fetching serialized(JSON) store state
+  static async getInitialProps(appContext: AppContext) {
+    const appProps = await App.getInitialProps(appContext);
+    const initialStoreState = await fetchInitialStoreState();
+
+    return {
+      ...appProps,
+      initialStoreState,
+    };
+  }
+
+  // Hydrate serialized state to store
+  static getDerivedStateFromProps(props: any, state: any) {
+    state.dataStore.hydrate(props.initialStoreState);
+    return state;
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <ChakraProvider theme={theme}>
+        <Provider dataStore={this.state.dataStore}>
+          <Component {...pageProps} />
+        </Provider>
+      </ChakraProvider>
+    );
+  }
 }
 
 export default MyApp;
---
+++ src/stores/DataStore.tsx
@@ -0,0 +1,27 @@
+import { observable } from "mobx";
+import { useStaticRendering } from "mobx-react";
+
+const isServer = typeof window === "undefined";
+useStaticRendering(isServer);
+
+type SerializedStore = {
+  title: string;
+  content: string;
+};
+
+export class DataStore {
+  @observable title: string | undefined;
+
+  hydrate(serializedStore: SerializedStore) {
+    this.title = serializedStore.title != null ? serializedStore.title : "";
+  }
+
+  changeTitle(newTitle: string) {
+    this.title = newTitle;
+  }
+}
+
+export async function fetchInitialStoreState() {
+  // You can do anything to fetch initial store state
+  return {};
+}
